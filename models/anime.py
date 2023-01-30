fb39e3be8bad666d2cbe2464ff57b599e0ceaa52
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-01-30 17:50:05 +0530

    Define anime model

---
+++ models/anime.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class Recommendation(BaseModel):
+    id: int
+    title: str
+    slug: str
+    poster: str | None
+    
+class Anime(BaseModel):
+    id: int
+    malId: int
+    kitsuId: int
+    title: str
+    en: str | None
+    enjp: str | None
+    jp: str | None
+    slug: str | None
+    poster: str | None
+    type: str | None
+    synopsis: str | None
+    status: str | None
+    genres: list[str] | None
+    tags: list[str] | None
+    season: str | None
+    startDate: datetime | None
+    endDate: datetime | None
+    rating: str | None
+    episodeCount: int | None
+    studio: list[str] | None
+    score: float | None
+    popularity: int | None
+    likes: int | None
+    recommended: list[Recommendation] | None
+    
+
+
