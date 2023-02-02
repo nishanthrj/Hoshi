4ebae1cb1ad2729c7d28d8e6522176c4659a4aec
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-02-02 13:24:14 +0530

    Implement manga search endpoint

--- routers/manga.py
+++ routers/manga.py
@@ -1,14 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query
 from fastapi.responses import JSONResponse
-from models.manga import Manga
+from models.manga import Manga, SearchResults
 from db import database
+from utils import build_pipeline
 
 manga = APIRouter()
 
 collection = database.get_collection('manga')
 
-@manga.get('/manga/{id}', response_model=Manga, response_model_by_alias=False)
-async def get_manga(id: int):
-    result = await collection.find_one({"_id": id})
+
+@manga.get('/manga', response_model=SearchResults, response_model_by_alias=False, tags=["Manga"])
+async def search_manga(q: str | None = None, subtype: str | None = None, status: str | None = None,
+                 year: str | None = None, tags: str | None = None, 
+                 exclude_genres: str | None = None, exclude_tags: str | None = None, 
+                 sort_by: str = 'score', sort_order: str = 'desc', page: int = 1):
+    
+    params = locals()
+    params['media_type'] = 'manga'
+    
+    print(build_pipeline(**params))
+    result = await collection.aggregate(build_pipeline(**params)).to_list(None)
+    
     return JSONResponse(content=result)
+
+
+@manga.get('/manga/{id}', response_model=Manga, response_model_by_alias=False, tags=["Manga"])
+async def get_manga(id: int):
+    return await collection.find_one({"_id":id})
     
