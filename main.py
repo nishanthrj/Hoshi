1909b2f177eebd556191e41461991f4053ecca00
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-02-02 13:24:41 +0530

    Add additional info to API docs

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from models.root import Root
 from routers.anime import anime
 from routers.manga import manga
 
+description = """
+Website: https://hoshi.ga	|	URL: https://api.hoshi.ga	|	License: [GPL-3.0](https://github.com/nishanthrj/Hoshi-API/blob/master/LICENSE)
 
-app = FastAPI()
+A simple REST API used in Hoshi.
+"""
+app = FastAPI(title='Hoshi API', version='1.0.0', description=description, redoc_url='/docs', docs_url=None)
 
 
 @app.get('/', response_model=Root, include_in_schema=False)
@@ -13,7 +18,7 @@ async def root():
 		'version': '0.1.0',
 		'website': 'https://hoshi.ga',
 		'author': 'https://github.com/nishanthrj',
-		'documentation': 'https://api.hoshi.ga/redoc',
+		'documentation': 'https://api.hoshi.ga/docs',
 		'github': 'https://github.com/nishanthrj/Hoshi',
 	}
     return data
