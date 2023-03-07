a0a0d4c48984a35093d96a15fa9a519b7a2ef32c
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-07 17:24:16 +0530

    Configure tailwind

--- core/app/layout.tsx
+++ core/app/layout.tsx
@@ -1,8 +1,17 @@
 import './globals.css'
+import { Overpass } from 'next/font/google';
 
 export const metadata = {
   title: 'Home',
 }
+
+
+const font = Overpass({
+  weight: ['300', '400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+  variable: '--font-overpass',
+  display: 'swap'
+})
 
 export default function RootLayout({
   children,
@@ -10,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${font.variable}`}>
       <body>{children}</body>
     </html>
   )
--- core/tailwind.config.js
+++ core/tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        dark: {
+          DEFAULT: '#110f19',
+          50: '#f8f8f8',
+          100: '#e1e1e2',
+          200: '#b3b3b6',
+          300: '#85848a',
+          400: '#58565e',
+          500: '#2a2832',
+          600: '#0d0c14',
+          700: '#0a090e',
+          800: '#060509',
+          900: '#020204',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-overpass)'],
+      },
+      screens: {
+        'max-xs': {'max': '425px'},
+      }
+    },
   },
   plugins: [],
 }
