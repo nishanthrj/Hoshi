86689520740a52d754998ed97b6a860e7f21f3aa
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-09 11:40:29 +0530

    Create search page

---
+++ core/app/search/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar"
+
+export default function Search() {
+  return (
+    <main className="grid grid-cols-[min-content_auto]">
+      <Navbar />
+    </main>
+  )
+}
