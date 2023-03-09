5f5bbe118c84b5623960d55efab089a303db1908
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-09 14:24:36 +0530

    Fix logout button alignment on small screen

--- core/components/Navbar.tsx
+++ core/components/Navbar.tsx
@@ -103,7 +103,7 @@ export default function Navbar() {
 								</span>
 							</Link>
 
-							<div className="flex pt-6 mt-auto mb-20 border-t justify-self-end border-dark-100/25">
+							<div className="flex pt-6 mt-auto mb-20 border-t max-xs:justify-between justify-self-end border-dark-100/25">
 								<div className="gap-3 font-semibold leading-[3rem] nav-link text-dark-100 hover:text-dark-100">
 									<div className="relative w-10 h-10">
 										<Image
@@ -118,9 +118,9 @@ export default function Navbar() {
 								</div>
 								<Link
 									href="/logout"
-									className="pt-[.85rem] ml-20 transition-colors duration-300 hover:text-dark-100"
+									className="pt-[.85rem] max-xs:mr-5 ml-20 transition-colors duration-300 hover:text-dark-100"
 								>
-									<MdLogout className="text-xl" />
+									<MdLogout className={`text-xl transition-opacity duration-300 ${isOpen ? "max-xs:opacity-100 delay-[700ms]" : "max-xs:opacity-0"}`} />
 								</Link>
 							</div>
 						</>
