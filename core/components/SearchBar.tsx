17330de3bbbcedc58296a1e8cde335ce8b4d753b
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-10 21:57:58 +0530

    Fix color in search bar

--- core/components/SearchBar.tsx
+++ core/components/SearchBar.tsx
@@ -2,15 +2,15 @@ import { CgSearch } from "react-icons/cg";
 
 export default function SearchBar() {
 	return (
-		<div className="mt-8 text-dark-100">
-			<span className="font-medium">Search</span>
-			<div className="mt-2 flex w-full items-center gap-2 rounded-md bg-dark-600 py-[.35rem] pl-3 text-dark-200 shadow">
+		<div>
+			<span className="font-medium text-dark-100">Search</span>
+			<div className="mt-2 flex items-center gap-2 rounded-md bg-dark-600 py-[.35rem] pl-3 text-dark-200 shadow">
 				<CgSearch className="h-5 w-5" />
 				<input
 					type="text"
 					autoComplete="off"
 					placeholder="Search"
-					className="w-full border-none bg-transparent py-[.35rem] text-sm font-medium tracking-wide outline-none placeholder:text-dark-300"
+					className="w-full border-none bg-transparent py-[.35rem] text-sm font-medium tracking-wide text-dark-100 outline-none placeholder:text-dark-300"
 				/>
 			</div>
 		</div>
