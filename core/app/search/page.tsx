1bac4585cbb0ba3c178f019c5e4ff0932defefa4
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-10 18:54:15 +0530

    Create search bar component

--- core/app/search/page.tsx
+++ core/app/search/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Navbar from "@/components/Navbar";
+import SearchBar from "@/components/SearchBar";
 import SearchHeader from "@/components/SearchHeader";
 
 export default function Search() {
@@ -9,6 +10,9 @@ export default function Search() {
 			<Navbar />
 			<section className="w-full p-4 mt-20">
 				<SearchHeader />
+				<div className="grid grid-cols-[repeat(auto-fit, 11rem)] gap-x-4 gap-y-8 w-full overflow-hidden">
+					<SearchBar />
+				</div>
 			</section>
 		</main>
 	);
