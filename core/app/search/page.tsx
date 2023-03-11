bc66bc44fc37f3d84b1f2a4b2049fbd2da85e8d8
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-11 13:14:53 +0530

    Add filter fields to the search page

--- core/app/search/page.tsx
+++ core/app/search/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 
+import FilterField from "@/components/FilterField";
 import Navbar from "@/components/Navbar";
 import SearchBar from "@/components/SearchBar";
 import SearchHeader from "@/components/SearchHeader";
 
 export default function Search() {
 	return (
-		<main className="grid grid-cols-[min-content_auto]">
+		<main className="mt-8 grid grid-cols-[min-content_auto]">
 			<Navbar />
-			<section className="mt-20 w-full p-4">
+			<section className="mt-12 w-full pl-4">
 				<SearchHeader />
-				<div className="grid-cols-[repeat(auto-fit, 11rem)] grid w-full gap-x-4 gap-y-8 overflow-hidden">
+				<div className="mt-14 grid w-full grid-cols-[repeat(auto-fit,100%)] gap-x-4 gap-y-8 overflow-hidden xl:grid-cols-[auto_min-content] xl:pr-4">
 					<SearchBar />
+					<div className="grid w-full grid-cols-[repeat(4,11rem)] gap-4">
+						<FilterField title="genres" />
+						<FilterField title="subtype" />
+						<FilterField title="status" />
+						<FilterField title="season" />
+					</div>
 				</div>
 			</section>
 		</main>
