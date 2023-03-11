46d1cb858447053a7073dfcbd58546cef40e8008
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-11 13:16:21 +0530

    Fix spacing on search bar

--- core/components/SearchBar.tsx
+++ core/components/SearchBar.tsx
@@ -2,9 +2,9 @@ import { CgSearch } from "react-icons/cg";
 
 export default function SearchBar() {
 	return (
-		<div>
-			<span className="font-medium text-dark-100">Search</span>
-			<div className="mt-2 flex items-center gap-2 rounded-md bg-dark-600 py-[.35rem] pl-3 text-dark-200 shadow">
+		<div className="pr-4 xl:pr-0">
+			<span className="mb-1 ml-2 text-sm font-medium text-dark-100">Search</span>
+			<div className="flex items-center gap-2 rounded-md bg-dark-600 py-[.35rem] pl-3 text-dark-200 shadow">
 				<CgSearch className="h-5 w-5" />
 				<input
 					type="text"
