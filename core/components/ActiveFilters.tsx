83b6e88f96ef6766ba077a1d6aca97987f7e0727
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-14 14:05:29 +0530

    Fix active tag icon position

--- core/components/ActiveFilters.tsx
+++ core/components/ActiveFilters.tsx
@@ -1,7 +1,7 @@
 import { AiFillTags } from "react-icons/ai";
 export default function ActiveFilters() {
 	return (
-		<div className="flex items-center gap-4 text-sm font-semibold text-dark-200">
+		<div className="flex gap-4 text-sm font-semibold text-dark-200">
 			<AiFillTags className="h-6 w-6" />
 			<div className="flex w-full flex-wrap gap-2 overflow-hidden">
 				<span className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
