190c81283c893cfca282c15230930dcb336aec77
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-14 12:40:34 +0530

    Fix bug in dropdown position

--- core/components/Dropdown.tsx
+++ core/components/Dropdown.tsx
@@ -10,7 +10,7 @@ export default function Dropdown({ name }: DropdownProps) {
 	const openDropdown = useSearchStore((state) => state.openDropdown);
 	return (
 		<div
-			className={`dropdown z-40 mb-2 mt-4 w-44 select-none overflow-y-hidden rounded-md bg-dark-600 text-sm font-semibold transition-[height] duration-300 hover:overflow-y-scroll ${
+			className={`dropdown absolute z-40 mt-4 w-44 select-none overflow-y-hidden rounded-md bg-dark-600 text-sm font-semibold transition-[height] duration-300 hover:overflow-y-scroll ${
 				openDropdown === name ? "h-80" : "h-0"
 			}`}>
 			<ul className="p-4">
