2bf02c71a80ee17ee39bbe746b5f446815363699
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-16 17:15:40 +0530

    Display sort method based on state

--- core/components/SortField.tsx
+++ core/components/SortField.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { MdUnfoldMore } from "react-icons/md";
 import Dropdown from "./Dropdown";
-import { useDropdownStore } from "@/app/store";
+import { useDropdownStore, useSearchStore } from "@/app/store";
 
 export default function SortField() {
 	const setOpenDropdown = useDropdownStore((state) => state.setOpenDropdown);
-
+	const sortMethod = useSearchStore((state) => state.sort);
 	return (
 		<div className="relative hidden w-28 justify-self-end xl:block">
 			<div
@@ -13,7 +13,7 @@ export default function SortField() {
 				className="flex cursor-pointer select-none items-center gap-1 whitespace-nowrap">
 				<MdUnfoldMore className="h-5 w-5 text-dark-200" />
 				<span className="select-none text-sm font-medium capitalize leading-none text-dark-100">
-					Title
+					{sortMethod}
 				</span>
 			</div>
 			<Dropdown name="sort" />
