0332dd47ef7e8ec76fac40adacf0360e37526f9b
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-16 17:15:57 +0530

    Create active filters component

--- core/components/ActiveFilters.tsx
+++ core/components/ActiveFilters.tsx
@@ -1,21 +1,77 @@
+"use client";
 import { AiFillTags } from "react-icons/ai";
+import { useSearchStore } from "@/app/store";
+import { v4 as uuid } from "uuid";
+
 export default function ActiveFilters() {
+	const [query, genres, excludedGenres, tags, excludedTags, format, status, release] =
+		useSearchStore((state) => [
+			state.q,
+			state.genres,
+			state.excludedGenres,
+			state.tags,
+			state.excludedTags,
+			state.format,
+			state.status,
+			state.release,
+		]);
+
 	return (
 		<div className="flex gap-4 text-sm font-semibold text-dark-200">
 			<AiFillTags className="h-6 w-6" />
 			<div className="flex w-full flex-wrap gap-2 overflow-hidden">
-				<span className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
-					Search: Bocchi
-				</span>
-				<span className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
-					Music
-				</span>
-				<span className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
-					Completed
-				</span>
-				<span className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
-					Fall 2022
-				</span>
+				{query && (
+					<span className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
+						Search: {query}
+					</span>
+				)}
+				{[...genres].map((genre) => (
+					<span
+						key={uuid()}
+						className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
+						{genre}
+					</span>
+				))}
+				{[...excludedGenres].map((exgenre) => (
+					<span
+						key={uuid()}
+						className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
+						~{exgenre}
+					</span>
+				))}
+				{[...tags].map((tag) => (
+					<span
+						key={uuid()}
+						className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
+						{tag}
+					</span>
+				))}
+				{[...excludedTags].map((extag) => (
+					<span
+						key={uuid()}
+						className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
+						~{extag}
+					</span>
+				))}
+				{[...format].map((format) => (
+					<span
+						key={uuid()}
+						className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
+						{format}
+					</span>
+				))}
+				{[...status].map((status) => (
+					<span
+						key={uuid()}
+						className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
+						{status}
+					</span>
+				))}
+				{release && (
+					<span className="select-none rounded bg-dark-400 p-2 text-xs leading-none text-dark-100">
+						{release}
+					</span>
+				)}
 			</div>
 		</div>
 	);
