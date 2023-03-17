3f3fbaf02830eb15182e4d78499e5af30b52715b
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-17 18:07:33 +0530

    Add extra colors to tailwind config

--- core/tailwind.config.js
+++ core/tailwind.config.js
@@ -1,33 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        dark: {
-          DEFAULT: '#110f19',
-          50: '#f8f7fd',
-          100: '#e2ddf2',
-          200: '#9c96b0',
-          300: '#646071',
-          400: '#292439',
-          500: '#201d2d',
-          600: '#1d1a28',
-          700: '#171520',
-          800: '#0e0c14',
-          900: '#0b090f',
-        },
-      },
-      fontFamily: {
-        sans: ['var(--font-overpass)'],
-      },
-      screens: {
-        'max-xs': {'max': '425px'},
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				dark: {
+					DEFAULT: "#110f19",
+					50: "#f8f7fd",
+					100: "#e2ddf2",
+					200: "#9c96b0",
+					300: "#646071",
+					400: "#292439",
+					500: "#201d2d",
+					600: "#1d1a28",
+					700: "#171520",
+					800: "#0e0c14",
+					900: "#0b090f",
+				},
+				extras: {
+					green: "#7bd555",
+					orange: "#f79a63",
+					red: "#e85d75",
+				},
+			},
+			fontFamily: {
+				sans: ["var(--font-overpass)"],
+			},
+			screens: {
+				"max-xs": { max: "425px" },
+			},
+		},
+	},
+	plugins: [],
+};
