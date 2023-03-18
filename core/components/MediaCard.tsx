5dcfb441b7058b10340b8f6037613968023e8ffa
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-18 18:43:58 +0530

    Fix minor bugs in media card component style

--- core/components/MediaCard.tsx
+++ core/components/MediaCard.tsx
@@ -33,18 +33,18 @@ export default function MediaCard({
 	const mediaType = useSearchStore((state) => state.mediaType);
 
 	return (
-		<div className="grid h-64 grid-cols-[11.5rem_auto] overflow-hidden rounded-md bg-dark-600 text-sm font-medium text-dark-200 shadow">
+		<div className="grid h-60 grid-cols-[10.75rem_auto] overflow-hidden rounded-md bg-dark-600 text-sm font-medium text-dark-200 shadow">
 			<div className="relative">
 				<Image src={poster} fill={true} quality={100} alt="cover" />
 			</div>
-			<div className="h-[95%] w-full overflow-hidden p-4">
-				<div className="flex w-full justify-between pr-2">
+			<div className="card-info h-56 w-full overflow-y-hidden p-4 pb-0 hover:overflow-y-auto">
+				<div className="relative flex w-full justify-between pr-2">
 					<Link
 						href={`/${mediaType}/${id}/${slug}`}
-						className="w-4/5 break-words text-base text-dark-50">
+						className="w-4/5 break-words text-base text-dark-50 line-clamp-2">
 						{title}
 					</Link>
-					<div className="flex items-center gap-x-2 text-[.9rem] font-semibold">
+					<div className="absolute left-72 flex items-center gap-x-2 text-[.9rem] font-semibold">
 						<FaRegSmile className="mb-[.15rem] h-5 w-5 text-extras-green" />
 						<span>{score}%</span>
 					</div>
@@ -58,12 +58,12 @@ export default function MediaCard({
 					{genres.map((genre) => (
 						<span
 							key={uuid()}
-							className="rounded-md bg-dark-400 px-[.6rem] py-[.35rem] text-xs">
+							className="self-start whitespace-nowrap rounded-md bg-dark-400 px-[.6rem] py-[.35rem] text-xs">
 							{genre}
 						</span>
 					))}
 				</div>
-				<p className="synopsis mt-4 h-[7.5rem] overflow-hidden text-[.7rem] font-normal leading-[1.1rem] hover:overflow-auto">
+				<p className="synopsis mt-4 text-[.7rem] font-normal leading-[1.6] line-clamp-6">
 					{synopsis}
 				</p>
 			</div>
