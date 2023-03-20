8ab12655da7d88e9b756fac0fa3a49d458c8821e
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-20 19:25:41 +0530

    Fix bug in logout button transition

--- core/components/NavProfile.tsx
+++ core/components/NavProfile.tsx
@@ -25,7 +25,7 @@ export default function NavProfile() {
 			</div>
 			<Link
 				href="/logout"
-				className="ml-24 pt-[.85rem] transition-colors duration-300 hover:text-dark-100 max-xs:mr-5">
+				className="ml-24 pt-[.85rem] transition-opacity duration-300 hover:text-dark-100 max-xs:mr-5">
 				<MdLogout
 					className={`text-xl transition-opacity duration-300 ${
 						isOpen ? "delay-[700ms] max-xs:opacity-100" : "max-xs:opacity-0"
