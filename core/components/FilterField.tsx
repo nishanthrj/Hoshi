f7a5aeac54babe65937f89a5139b3e981861aebe
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-21 18:39:01 +0530

    Modify filter field component

--- core/components/FilterField.tsx
+++ core/components/FilterField.tsx
@@ -1,18 +1,30 @@
+"use client";
 import ActiveFilterMini from "./ActiveFilterMini";
 import Dropdown from "./Dropdown";
-import Field from "./DropdownField";
+import { MdExpandMore, MdExpandLess } from "react-icons/md";
+import { useDropdownStore } from "@/stores/common";
 
 interface FilterFieldProps {
 	name: string;
 }
 
 export default function FilterField({ name }: FilterFieldProps) {
+	const openDropdown = useDropdownStore((state) => state.openDropdown);
+	const setOpenDropdown = useDropdownStore((state) => state.setOpenDropdown);
+
 	return (
 		<div className="relative snap-center">
 			<span className="mb-1 ml-2 text-sm font-medium capitalize text-dark-100">{name}</span>
-			<Field name={name}>
+			<div
+				onClick={() => setOpenDropdown(name)}
+				className="flex items-center justify-between rounded-md bg-dark-600 py-[.45rem] px-3 text-dark-200 shadow">
 				<ActiveFilterMini name={name} />
-			</Field>
+				{openDropdown === name ? (
+					<MdExpandMore className="h-5 w-5" />
+				) : (
+					<MdExpandLess className="h-5 w-5" />
+				)}
+			</div>
 			<Dropdown name={name} />
 		</div>
 	);
