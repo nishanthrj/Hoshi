0854180940c6cb9d587f8cd58cf42c5714e48dc1
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-21 13:05:52 +0530

    Convert navbar into a server component

--- core/components/navbar/NavProfile.tsx
+++ core/components/navbar/NavProfile.tsx
@@ -1,13 +1,7 @@
-"use client";
-
-import Link from "next/link";
 import Image from "next/image";
-import { MdLogout } from "react-icons/md";
-import { useNavbarStore } from "@/stores/navbar";
+import LogoutButton from "./LogoutButton";
 
 export default function NavProfile() {
-	const isOpen = useNavbarStore((state) => state.isOpen);
-
 	return (
 		<div className="mt-auto mb-[6.5rem] flex justify-self-end border-t border-dark-100/25 pt-6 max-xs:justify-between">
 			<div className="inline-flex gap-3 pl-2 font-semibold leading-[3rem] text-dark-100 hover:text-dark-100">
@@ -23,15 +17,7 @@ export default function NavProfile() {
 				</div>
 				Guest
 			</div>
-			<Link
-				href="/logout"
-				className="ml-24 pt-[.85rem] transition-opacity duration-300 hover:text-dark-100 max-xs:mr-5">
-				<MdLogout
-					className={`text-xl transition-opacity duration-300 ${
-						isOpen ? "delay-[700ms] max-xs:opacity-100" : "max-xs:opacity-0"
-					}`}
-				/>
-			</Link>
+			<LogoutButton />
 		</div>
 	);
 }
--- core/components/navbar/Navbar.tsx
+++ core/components/navbar/Navbar.tsx
@@ -1,8 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
-
 import {
 	MdHome,
 	MdSettings,
@@ -13,62 +8,32 @@ import {
 	MdLogin,
 	MdAutoGraph,
 } from "react-icons/md";
-
-import MenuIcon from "./NavIcon";
 import NavLink from "./NavLink";
 import NavProfile from "./NavProfile";
-import Logo from "./Logo";
-import { useNavbarStore } from "@/stores/navbar";
+import NavbarWrapper from "./NavbarWrapper";
 
 export default function Navbar() {
-	const pathname = usePathname();
-	const isOpen = useNavbarStore((state) => state.isOpen);
-	const closeNavbar = useNavbarStore((state) => state.closeNavbar);
 	const loggedIn = true;
-
-	useEffect(() => closeNavbar(), [pathname]);
-
-	useEffect(() => {
-		isOpen
-			? document
-					.querySelector("body")
-					?.classList.add("overflow-y-hidden", "md:overflow-y-visible")
-			: document
-					.querySelector("body")
-					?.classList.remove("overflow-y-hidden", "md:overflow-y-visible");
-	}, [isOpen]);
-
 	return (
-		<div
-			className={`z-[100] md:transition-[width] md:duration-1000 ${
-				isOpen ? "md:w-64" : "md:w-16"
-			}`}>
-			<MenuIcon />
-			<div
-				className={`fixed top-0 left-0 min-h-screen overflow-hidden border-r border-dark-100/25 bg-dark/75 font-sans backdrop-blur transition-[width] duration-1000 ${
-					isOpen ? "w-64 max-xs:w-full" : "w-0 md:w-16"
-				}`}>
-				<Logo />
+		<NavbarWrapper>
+			<nav className="mt-8 ml-2 flex h-screen flex-col whitespace-nowrap text-dark-200 transition-colors duration-300">
+				<NavLink name="Home" path="/" Icon={MdHome} />
+				<NavLink name="Search" path="/search" Icon={MdSearch} />
+				<NavLink name="Explore" path="/explore" Icon={MdExplore} />
 
-				<nav className="mt-8 ml-2 flex h-screen flex-col whitespace-nowrap text-dark-200 transition-colors duration-300">
-					<NavLink name="Home" path="/" Icon={MdHome} />
-					<NavLink name="Search" path="/search" Icon={MdSearch} />
-					<NavLink name="Explore" path="/explore" Icon={MdExplore} />
+				{loggedIn ? (
+					<>
+						<NavLink name="AnimeList" path="/user/animelist" Icon={MdPlayArrow} />
+						<NavLink name="MangaList" path="/user/mangalist" Icon={MdMenuBook} />
+						<NavLink name="Stats" path="/user/stats" Icon={MdAutoGraph} />
+						<NavLink name="Settings" path="/user/settings" Icon={MdSettings} />
 
-					{loggedIn ? (
-						<>
-							<NavLink name="AnimeList" path="/user/animelist" Icon={MdPlayArrow} />
-							<NavLink name="MangaList" path="/user/mangalist" Icon={MdMenuBook} />
-							<NavLink name="Stats" path="/user/stats" Icon={MdAutoGraph} />
-							<NavLink name="Settings" path="/user/settings" Icon={MdSettings} />
-
-							<NavProfile />
-						</>
-					) : (
-						<NavLink name="Login" path="/login" Icon={MdLogin} />
-					)}
-				</nav>
-			</div>
-		</div>
+						<NavProfile />
+					</>
+				) : (
+					<NavLink name="Login" path="/login" Icon={MdLogin} />
+				)}
+			</nav>
+		</NavbarWrapper>
 	);
 }
