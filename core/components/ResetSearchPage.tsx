1980b9babd7f5c24eea3922bd909e684331efd29
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-21 14:26:42 +0530

    Remove unnecessary variables in ResetSearchPage

--- core/components/ResetSearchPage.tsx
+++ core/components/ResetSearchPage.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 import { useDropdownStore } from "@/stores/common";
 import { useSearchStore } from "@/stores/search";
 
 export default function ResetSearchPage() {
-	const pathname = usePathname();
 	const reset = useSearchStore((state) => state.reset);
-	const genres = useSearchStore((state) => state.genres);
 	const setOpenDropdown = useDropdownStore((state) => state.setOpenDropdown);
 
 	useEffect(() => {
