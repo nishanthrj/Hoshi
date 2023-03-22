b3f4fe0b55428631a9e5e13008b07d664a866ae6
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-22 11:23:24 +0530

    Create anime page

---
+++ core/app/anime/[id]/[[...slug]]/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/navbar/Navbar";
+
+export default function Anime() {
+	return (
+		<main className="grid grid-cols-[min-content_auto]">
+			<Navbar />
+			<section className="mt-12 w-full pl-4"></section>
+		</main>
+	);
+}
