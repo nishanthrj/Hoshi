8e10b0ab210e27611243996ee239060bf4b10537
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-23 12:03:31 +0530

    Fix relation style for small screens

--- core/components/media/OverviewTab.tsx
+++ core/components/media/OverviewTab.tsx
@@ -2,13 +2,11 @@ import Relation from "@/components/media/Relation";
 
 export default function OverviewTab() {
 	return (
-		<div className="mt-20 ml-10 w-full">
-			<h1 className="mb-4 text-xl font-semibold uppercase tracking-widest text-dark-100">
+		<div className="mt-20 mb-20 w-full md:ml-5">
+			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Relations
 			</h1>
-			<div className="grid grid-cols-[repeat(auto-fill,30rem)] gap-8 overflow-hidden">
-				<Relation />
-				<Relation />
+			<div className="grid grid-cols-[repeat(auto-fill,min(25rem,95%))] gap-8 overflow-hidden">
 				<Relation />
 				<Relation />
 			</div>
--- core/components/media/Relation.tsx
+++ core/components/media/Relation.tsx
@@ -3,24 +3,24 @@ import MediaImage from "../common/MediaImage";
 
 export default function Relation() {
 	return (
-		<div className="grid h-32 grid-cols-[6.5rem_auto] gap-4 overflow-hidden rounded bg-dark-600 leading-5 text-dark-200">
+		<div className="grid h-32 grid-cols-[6rem_auto] gap-4 overflow-hidden rounded bg-dark-600 leading-5 text-dark-200 max-xs:h-24 max-xs:grid-cols-[5rem_auto]">
 			<div className="relative">
 				<MediaImage
 					src="https://media.kitsu.io/anime/44196/poster_image/large-04fc7c066e52d4d2b9d0217b383597bb.jpeg"
 					fill={true}
 					style={{ objectFit: "cover" }}
 					quality={100}
-					sizes="104px"
+					sizes="96px"
 					alt="cover"
 					className="rounded-sm"
 				/>
 			</div>
-			<div className="mx-3 flex flex-col pt-3 pb-2 capitalize">
+			<div className="flex flex-col pt-3 pb-2 capitalize">
 				<span className="text-xs font-medium">Adaptation</span>
-				<Link href="" className="mt-1 text-dark-50 line-clamp-2">
+				<Link href="" className="mt-1 text-dark-50 line-clamp-2 max-xs:text-sm">
 					Bocchi the Rock!
 				</Link>
-				<span className="mt-auto text-xs font-normal">Manga • Ongoing</span>
+				<span className="mt-auto text-xs font-medium">Manga • Ongoing</span>
 			</div>
 		</div>
 	);
