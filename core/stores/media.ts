83c0583f724dabdf53374a3b634fe9d47ecbef96
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-24 21:48:47 +0530

    Remove null state from id

--- core/stores/media.ts
+++ core/stores/media.ts
@@ -2,18 +2,18 @@ import { create } from "zustand";
 
 interface MediaStore {
 	mediaType: string;
-	mediaId: number | null;
-	malId: number | null;
-	kitsuId: number | null;
+	mediaId: number;
+	malId: number;
+	kitsuId: number;
 	activeTab: string;
 	setActiveTab(tab: string): void;
 }
 
 export const useMediaStore = create<MediaStore>((set) => ({
 	mediaType: "anime",
-	mediaId: null,
-	malId: null,
-	kitsuId: null,
+	mediaId: 0,
+	malId: 0,
+	kitsuId: 0,
 	activeTab: "overview",
 	setActiveTab: (tab) => set(() => ({ activeTab: tab })),
 }));
