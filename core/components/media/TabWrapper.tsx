3e4f1ce048695ca83a5085010972b03acef2f573
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-24 18:01:11 +0530

    Center align all the sections

--- core/components/media/TabWrapper.tsx
+++ core/components/media/TabWrapper.tsx
@@ -10,5 +10,10 @@ interface TabWrapper {
 export default function TabWrapper({ children, name }: TabWrapper) {
 	const activeTab = useMediaStore((state) => state.activeTab);
 
-	return <section className={`${activeTab === name ? "block" : "hidden"}`}>{children}</section>;
+	return (
+		<section
+			className={`${activeTab === name ? "block" : "hidden"} flex flex-col items-center`}>
+			{children}
+		</section>
+	);
 }
