030e05bc8fe6a32ea1c9edb93b3a4ad07f674de9
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-24 17:48:34 +0530

    Create voice actor card component

--- core/components/media/StaffTab.tsx
+++ core/components/media/StaffTab.tsx
@@ -1,16 +1,29 @@
 import StaffCard from "./StaffCard";
+import VoiceActorCard from "./VoiceActorCard";
 
 export default function StaffTab() {
 	return (
-		<div className="mt-20 w-[min(70rem,100%)] md:ml-5">
-			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
-				Staff
-			</h1>
-			<div className="grid grid-cols-[repeat(auto-fill,min(22rem,100%))] gap-8 overflow-hidden">
-				<StaffCard />
-				<StaffCard />
-				<StaffCard />
+		<>
+			<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
+				<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
+					Voice Actors
+				</h1>
+				<div className="grid grid-cols-[repeat(auto-fill,min(32rem,100%))] gap-8 overflow-hidden">
+					<VoiceActorCard />
+					<VoiceActorCard />
+					<VoiceActorCard />
+				</div>
 			</div>
-		</div>
+			<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
+				<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
+					Staff
+				</h1>
+				<div className="grid grid-cols-[repeat(auto-fill,min(21rem,100%))] gap-8 overflow-hidden">
+					<StaffCard />
+					<StaffCard />
+					<StaffCard />
+				</div>
+			</div>
+		</>
 	);
 }
---
+++ core/components/media/VoiceActorCard.tsx
@@ -0,0 +1,44 @@
+import MediaImage from "../common/MediaImage";
+
+export default function VoiceActorCard() {
+	return (
+		<div className="grid grid-cols-2">
+			<div className="grid h-24 grid-cols-[4.25rem_auto] gap-4 overflow-hidden rounded-l bg-dark-600 leading-5 text-dark-200">
+				<div className="relative">
+					<MediaImage
+						src="https://cdn.myanimelist.net/images/voiceactors/2/73098.jpg"
+						fill={true}
+						style={{ objectFit: "cover" }}
+						quality={100}
+						sizes="96px"
+						alt="cover"
+						className="rounded-sm"
+					/>
+				</div>
+				<div className="flex flex-col gap-2 pt-3 capitalize">
+					<p className="mt-1 text-dark-50 line-clamp-2 max-xs:text-xs">Shouta Umehara</p>
+					<p className="text-xs font-medium line-clamp-2">Producer</p>
+				</div>
+			</div>
+			<div className="grid h-24 grid-cols-[auto_4.25rem] gap-4 overflow-hidden rounded-r bg-dark-600 leading-5 text-dark-200">
+				<div className="flex flex-col gap-2 pt-3 capitalize">
+					<p className="mt-1 text-right text-dark-50 line-clamp-2 max-xs:text-xs">
+						Shouta Umehara
+					</p>
+					<p className="text-right text-xs font-medium line-clamp-2">Producer</p>
+				</div>
+				<div className="relative">
+					<MediaImage
+						src="https://cdn.myanimelist.net/images/voiceactors/2/73098.jpg"
+						fill={true}
+						style={{ objectFit: "cover" }}
+						quality={100}
+						sizes="96px"
+						alt="cover"
+						className="rounded-sm"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
