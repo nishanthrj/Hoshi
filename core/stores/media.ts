943aeb1e10a0419b2c79df54e69f27fce005d9ed
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-24 19:18:38 +0530

    Add mediaId to the media store

--- core/stores/media.ts
+++ core/stores/media.ts
@@ -2,12 +2,14 @@ import { create } from "zustand";
 
 interface MediaStore {
 	mediaType: string;
+	mediaId: number | null;
 	activeTab: string;
 	setActiveTab(tab: string): void;
 }
 
 export const useMediaStore = create<MediaStore>((set) => ({
 	mediaType: "anime",
+	mediaId: null,
 	activeTab: "overview",
 	setActiveTab: (tab) => set(() => ({ activeTab: tab })),
 }));
