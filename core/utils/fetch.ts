3691af388b3c031c9f3ffc1e84f7b47f5015c615
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-24 19:19:27 +0530

    Create a function to fetch anime data

--- core/utils/fetch.ts
+++ core/utils/fetch.ts
@@ -4,3 +4,11 @@ export const getSearchResults = (page: number, mediaType: string, filters: any) 
 	axios
 		.get(`http://127.0.0.1:8000/${mediaType}`, { params: { page: page, ...filters } })
 		.then((res) => res.data);
+
+export const getAnime = async (id: number) => {
+	const res = await fetch(`http://127.0.0.1:8000/anime/${id}`, { next: { revalidate: 20 * 60 } });
+	if (!res.ok) {
+		throw new Error("Failed to fetch data");
+	}
+	return res.json();
+};
