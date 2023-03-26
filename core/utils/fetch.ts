450952676e8b894e76750d3f6552d5d1bb815399
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-26 12:20:31 +0530

    Implement function to fetch related media

--- core/utils/fetch.ts
+++ core/utils/fetch.ts
@@ -12,3 +12,45 @@ export const getAnime = async (id: number) => {
 	}
 	return res.json();
 };
+
+export const getRelatedAnime = async (kitsuId: number) => {
+	const results: any = [];
+	const relationship: any = [];
+
+	const res = await fetch(
+		`https://kitsu.io/api/edge/anime/${kitsuId}?include=mediaRelationships.destination`,
+		{ next: { revalidate: 2 * 60 * 60 } },
+	);
+
+	if (!res.ok) {
+		throw new Error("Failed to fetch data");
+	}
+
+	const data = await res.json();
+
+	data.included.forEach((entry: any) => {
+		if (entry.type === "anime" || entry.type === "manga") {
+			results.push({
+				id: entry.id,
+				poster: entry.attributes.posterImage.original,
+				title: entry.attributes.canonicalTitle,
+				format: entry.attributes.subtype,
+				status: entry.attributes.status,
+			});
+			return;
+		}
+
+		if (entry.type === "mediaRelationships") {
+			relationship.push({
+				id: entry.relationships.destination.data.id,
+				role: entry.attributes.role.replace("_", " "),
+			});
+		}
+	});
+
+	results.forEach((x: any, i: any) => {
+		results[i].relation = relationship.find((r: any) => r.id === x.id).role;
+	});
+
+	return results.reverse();
+};
