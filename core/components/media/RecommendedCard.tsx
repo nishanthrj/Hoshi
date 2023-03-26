15a627cbfd74945ee848738fe45d06989c809d1b
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-26 19:16:51 +0530

    Update recommended section to use data from api

--- core/components/media/RecommendedCard.tsx
+++ core/components/media/RecommendedCard.tsx
@@ -1,25 +1,32 @@
 import Link from "next/link";
 import MediaImage from "@/components/common/MediaImage";
 
-export default function RecommendedCard() {
+interface RecommendedCardProps {
+	id: number;
+	slug: string;
+	title: string;
+	poster: string;
+}
+
+export default function RecommendedCard({ id, slug, title, poster }: RecommendedCardProps) {
 	return (
 		<div className="w-36 max-xs:w-32">
 			<div className="relative h-[12.65rem] max-xs:h-[11.25rem]">
 				<MediaImage
-					src="https://media.kitsu.io/anime/44196/poster_image/large-04fc7c066e52d4d2b9d0217b383597bb.jpeg"
+					src={poster}
 					fill={true}
 					style={{ objectFit: "cover" }}
 					quality={100}
-					sizes="144px"
+					sizes="200px"
 					alt="cover"
 					className="rounded-sm"
 				/>
 			</div>
 			<Link
-				href=""
+				href={`/${id}/${slug}`}
 				prefetch={false}
 				className="mt-2 text-sm font-semibold text-dark-100 line-clamp-2 max-xs:text-xs">
-				Bocchi the Rock!
+				{title}
 			</Link>
 		</div>
 	);
--- core/components/media/RecommendedSection.tsx
+++ core/components/media/RecommendedSection.tsx
@@ -1,24 +1,26 @@
 import RecommendedCard from "@/components/media/RecommendedCard";
+import { getAnime } from "@/utils/fetch";
+import { useMediaStore } from "@/stores/media";
 
-export default function RecommendedSection() {
+export default async function RecommendedSection() {
+	const id = useMediaStore.getState().mediaId;
+	const media = await getAnime(id);
+
 	return (
 		<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
 			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Recommended
 			</h1>
 			<div className="flex flex-wrap gap-8">
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
-				<RecommendedCard />
+				{media.recommended.map((rec: any) => (
+					<RecommendedCard
+						key={rec.id}
+						id={rec.id}
+						title={rec.title}
+						slug={rec.slug}
+						poster={rec.poster}
+					/>
+				))}
 			</div>
 		</div>
 	);
