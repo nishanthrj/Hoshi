2dfc38f953479e5ba2b946bff212ea89793d8af6
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-26 12:02:12 +0530

    Fix clear all button text position

--- core/components/search/ClearAllButton.tsx
+++ core/components/search/ClearAllButton.tsx
@@ -8,12 +8,10 @@ export default function ClearAllButton() {
 	const reset = useSearchStore((state) => state.reset);
 
 	return (
-		<span onClick={reset}>
-			<Tag className="transition-colors duration-200 hover:text-rose-500">
-				<span className="relative inline-flex max-h-2 w-16 gap-2">
-					Clear All <MdClose className="absolute right-0 -bottom-[.2rem]" />
-				</span>
-			</Tag>
-		</span>
+		<Tag className="cursor-pointer transition-colors duration-200 hover:text-rose-500">
+			<span onClick={reset} className="relative inline-flex max-h-2 w-16 gap-2">
+				Clear All <MdClose className="absolute right-0 -bottom-[.2rem]" />
+			</span>
+		</Tag>
 	);
 }
