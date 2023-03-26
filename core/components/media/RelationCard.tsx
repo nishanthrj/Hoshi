1f093b0428a67bd4b3c434378aef0188c595b08d
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-26 12:20:46 +0530

    Update relation section to use data from the api

--- core/components/media/RelationCard.tsx
+++ core/components/media/RelationCard.tsx
@@ -1,12 +1,28 @@
 import Link from "next/link";
 import MediaImage from "../common/MediaImage";
 
-export default function RelationCard() {
+interface RelationCardProps {
+	id: number;
+	title: string;
+	poster: string;
+	relation: string;
+	format: string;
+	status: string;
+}
+
+export default function RelationCard({
+	id,
+	title,
+	poster,
+	relation,
+	format,
+	status,
+}: RelationCardProps) {
 	return (
 		<div className="grid h-32 grid-cols-[6rem_auto] gap-4 overflow-hidden rounded bg-dark-600 leading-5 text-dark-200 max-xs:h-24 max-xs:grid-cols-[5rem_auto]">
 			<div className="relative">
 				<MediaImage
-					src="https://media.kitsu.io/anime/44196/poster_image/large-04fc7c066e52d4d2b9d0217b383597bb.jpeg"
+					src={poster}
 					fill={true}
 					style={{ objectFit: "cover" }}
 					quality={100}
@@ -16,11 +32,13 @@ export default function RelationCard() {
 				/>
 			</div>
 			<div className="flex flex-col pt-3 pb-2 capitalize">
-				<span className="text-xs font-medium">Adaptation</span>
+				<span className="text-xs font-medium">{relation}</span>
 				<Link href="" className="mt-1 text-dark-50 line-clamp-2 max-xs:text-sm">
-					Bocchi the Rock!
+					{title}
 				</Link>
-				<span className="mt-auto text-xs font-medium">Manga • Ongoing</span>
+				<span className="mt-auto text-xs font-medium">
+					{format} • {status}
+				</span>
 			</div>
 		</div>
 	);
--- core/components/media/RelationSection.tsx
+++ core/components/media/RelationSection.tsx
@@ -1,14 +1,30 @@
 import RelationCard from "@/components/media/RelationCard";
+import { getRelatedAnime, getAnime } from "@/utils/fetch";
+import { useMediaStore } from "@/stores/media";
+import { v4 as uuid } from "uuid";
 
-export default function RelationSection() {
+export default async function RelationSection() {
+	const id = useMediaStore.getState().mediaId;
+	const media = await getAnime(id);
+	const data = await getRelatedAnime(media.kitsuId);
+
 	return (
 		<div className="mt-20 w-[min(70rem,100%)] md:ml-5">
 			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Relations
 			</h1>
 			<div className="grid grid-cols-[repeat(auto-fill,min(25rem,100%))] gap-8 overflow-hidden">
-				<RelationCard />
-				<RelationCard />
+				{data.map((media: any) => (
+					<RelationCard
+						key={uuid()}
+						id={media.id}
+						title={media.title}
+						poster={media.poster}
+						relation={media.relation}
+						format={media.format}
+						status={media.status}
+					/>
+				))}
 			</div>
 		</div>
 	);
