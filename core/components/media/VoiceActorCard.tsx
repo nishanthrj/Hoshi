15df8251d39441c5ab209e65d02456a8aa5abf36
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-27 11:33:31 +0530

    Update voice actors section to use data from api

--- core/components/media/VoiceActorCard.tsx
+++ core/components/media/VoiceActorCard.tsx
@@ -1,12 +1,28 @@
 import MediaImage from "../common/MediaImage";
 
-export default function VoiceActorCard() {
+interface VoiceActorCardProps {
+	charaName: string;
+	voiceName: string;
+	charaImage: string;
+	voiceImage: string;
+	charaRole: string;
+	voiceRole: string;
+}
+
+export default function VoiceActorCard({
+	charaName,
+	voiceName,
+	charaImage,
+	voiceImage,
+	charaRole,
+	voiceRole,
+}: VoiceActorCardProps) {
 	return (
 		<div className="grid grid-cols-2">
 			<div className="grid h-24 grid-cols-[4.25rem_auto] gap-4 overflow-hidden rounded-l bg-dark-600 leading-5 text-dark-200">
 				<div className="relative">
 					<MediaImage
-						src="https://cdn.myanimelist.net/images/voiceactors/2/73098.jpg"
+						src={charaImage}
 						fill={true}
 						style={{ objectFit: "cover" }}
 						quality={100}
@@ -16,20 +32,20 @@ export default function VoiceActorCard() {
 					/>
 				</div>
 				<div className="flex flex-col gap-2 pt-3 capitalize">
-					<p className="mt-1 text-dark-50 line-clamp-2 max-xs:text-xs">Shouta Umehara</p>
-					<p className="text-xs font-medium line-clamp-2">Producer</p>
+					<p className="mt-1 text-dark-50 line-clamp-2 max-xs:text-xs">{charaName}</p>
+					<p className="text-xs font-medium line-clamp-2">{charaRole}</p>
 				</div>
 			</div>
 			<div className="grid h-24 grid-cols-[auto_4.25rem] gap-4 overflow-hidden rounded-r bg-dark-600 leading-5 text-dark-200">
 				<div className="flex flex-col gap-2 pt-3 capitalize">
 					<p className="mt-1 text-right text-dark-50 line-clamp-2 max-xs:text-xs">
-						Shouta Umehara
+						{voiceName}
 					</p>
-					<p className="text-right text-xs font-medium line-clamp-2">Producer</p>
+					<p className="text-right text-xs font-medium line-clamp-2">{voiceRole}</p>
 				</div>
 				<div className="relative">
 					<MediaImage
-						src="https://cdn.myanimelist.net/images/voiceactors/2/73098.jpg"
+						src={voiceImage}
 						fill={true}
 						style={{ objectFit: "cover" }}
 						quality={100}
--- core/components/media/VoiceActorsSection.tsx
+++ core/components/media/VoiceActorsSection.tsx
@@ -1,15 +1,30 @@
 import VoiceActorCard from "./VoiceActorCard";
+import { getCharacters, getAnime } from "@/utils/fetch";
+import { useMediaStore } from "@/stores/media";
+import { v4 as uuid } from "uuid";
 
-export default function VoiceActorsSection() {
+export default async function VoiceActorsSection() {
+	const id = useMediaStore.getState().mediaId;
+	const media = await getAnime(id);
+	const data = await getCharacters(media.malId);
+
 	return (
 		<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
 			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Voice Actors
 			</h1>
 			<div className="grid grid-cols-[repeat(auto-fill,min(32rem,100%))] gap-8 overflow-hidden">
-				<VoiceActorCard />
-				<VoiceActorCard />
-				<VoiceActorCard />
+				{data.map((chara: any) => (
+					<VoiceActorCard
+						key={uuid()}
+						charaName={chara.character.name.replace(",", "")}
+						voiceName={chara.voice_actors[0].person.name.replace(",", "")}
+						charaImage={chara.character.images.jpg.image_url}
+						voiceImage={chara.voice_actors[0].person.images.jpg.image_url}
+						charaRole={chara.role}
+						voiceRole={chara.voice_actors[0].language}
+					/>
+				))}
 			</div>
 		</div>
 	);
