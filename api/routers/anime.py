fcb91c1d473d3ca82460f8f45d12c30be04e7e09
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-27 19:07:52 +0530

    Migrate external fetch requests to main api

--- api/routers/anime.py
+++ api/routers/anime.py
@@ -3,10 +3,15 @@ from models.anime import Anime, SearchResults
 from db import database
 from utils import build_pipeline
 from fastapi.responses import JSONResponse
+from aiohttp_retry import RetryClient, ExponentialRetry
+
 
 anime = APIRouter()
 
 collection = database.get_collection('anime')
+
+retry_options = ExponentialRetry(attempts=3, start_timeout=1, factor=2.0, statuses={429, 500})
+client = RetryClient(raise_for_status=False, retry_options=retry_options)
 
 
 @anime.get('/anime', response_model=SearchResults, response_model_by_alias=False, tags=['Anime'])
@@ -28,7 +33,80 @@ async def get_anime(id: int):
     result = await collection.find_one({"_id": id})
     return JSONResponse(content=result)
 
+
 @anime.get('/anime/relation/{kitsuId}', response_model=Anime, response_model_by_alias=False, tags=['Anime'])
 async def get_anime_by_relation(kitsuId: int):
     result = await collection.find_one({"kitsuId": kitsuId})
     return JSONResponse(content=result)
+
+
+@anime.get('/anime/{kitsuId}/relation')
+async def get_anime_characters(kitsuId: int):
+    res = await client.get(f"https://kitsu.io/api/edge/anime/{kitsuId}?include=mediaRelationships.destination")
+    data = await res.json()
+    
+    results = []
+    relationship = []
+    
+    for entry in data["included"]:
+        if entry["type"] == "anime" or entry["type"] == "manga":
+            result = {
+                "id": entry["id"],
+                "type": entry["type"],
+                "poster": entry["attributes"].get("posterImage", {}).get("original"),
+                "title": entry["attributes"].get("canonicalTitle"),
+                "format": entry["attributes"].get("subtype"),
+                "status": entry["attributes"].get("status")
+            }
+            results.append(result)
+            continue
+
+        if entry["type"] == "mediaRelationships":
+            rel = {
+                "id": entry["relationships"]["destination"]["data"]["id"],
+                "role": entry["attributes"].get("role", "").replace("_", " ")
+            }
+            relationship.append(rel)
+
+    for i, x in enumerate(results):
+        for rel_dict in relationship:
+            if rel_dict["id"] == x["id"]:
+                results[i]["relation"] = rel_dict.get("role", None)
+                break
+        else:
+            results[i]["relation"] = None
+       
+    return JSONResponse(content=results)
+
+
+@anime.get('/anime/{malId}/characters')
+async def get_anime_characters(malId: int):
+    res = await client.get(f"https://api.jikan.moe/v4/anime/{malId}/characters")
+    data = await res.json()
+    data = sorted(data['data'], key=lambda x: x['favorites'], reverse=True);
+    return JSONResponse(content=data)
+
+
+@anime.get('/anime/{malId}/trailer')
+async def get_anime_trailer(malId: int):
+    res = await client.get(f"https://api.jikan.moe/v4/anime/{malId}/videos")
+    data = await res.json()
+    for promo in data.get('data').get('promo'):
+        if "PV" in promo['title'] and "Character" not in promo['title']:
+            return JSONResponse(content=promo)
+
+    return JSONResponse(content=data.get('data').get('promo')[0] if data.get('data') else None)
+
+
+@anime.get('/anime/{malId}/stats')
+async def get_anime_stats(malId: int):
+    res = await client.get(f"https://api.jikan.moe/v4/anime/{malId}/statistics")
+    data = await res.json()
+    return JSONResponse(content=data)
+
+
+@anime.get('/anime/{malId}/staff')
+async def get_anime_staff(malId: int):
+    res = await client.get(f"https://api.jikan.moe/v4/anime/{malId}/staff")
+    data = await res.json()
+    return JSONResponse(content=data)
--- core/utils/fetch.ts
+++ core/utils/fetch.ts
@@ -13,52 +13,19 @@ export const getAnime = async (id: number) => {
 	return res.json();
 };
 
-export const getRelatedAnime = async (kitsuId: number) => {
-	const results: any = [];
-	const relationship: any = [];
-
-	const res = await fetch(
-		`https://kitsu.io/api/edge/anime/${kitsuId}?include=mediaRelationships.destination`,
-		{ next: { revalidate: 2 * 60 * 60 } },
-	);
-
+export const getAnimeFromRelation = async (kitsuId: number) => {
+	const res = await fetch(`http://127.0.0.1:8000/anime/relation/${kitsuId}`, {
+		cache: "no-cache",
+	});
 	if (!res.ok) {
 		throw new Error("Failed to fetch data");
 	}
-
-	const data = await res.json();
-
-	data.included.forEach((entry: any) => {
-		if (entry.type === "anime" || entry.type === "manga") {
-			results.push({
-				id: entry.id,
-				type: entry.type,
-				poster: entry.attributes.posterImage.original,
-				title: entry.attributes.canonicalTitle,
-				format: entry.attributes.subtype,
-				status: entry.attributes.status,
-			});
-			return;
-		}
-
-		if (entry.type === "mediaRelationships") {
-			relationship.push({
-				id: entry.relationships.destination.data.id,
-				role: entry.attributes.role.replace("_", " "),
-			});
-		}
-	});
-
-	results.forEach((x: any, i: any) => {
-		results[i].relation = relationship.find((r: any) => r.id === x.id).role;
-	});
-
-	return results.reverse();
+	return res.json();
 };
 
-export const getAnimeFromRelation = async (kitsuId: number) => {
-	const res = await fetch(`http://127.0.0.1:8000/anime/relation/${kitsuId}`, {
-		next: { revalidate: 2 * 60 * 60 },
+export const getRelatedAnime = async (kitsuId: number) => {
+	const res = await fetch(`http://127.0.0.1:8000/anime/${kitsuId}/relation`, {
+		cache: "no-cache",
 	});
 	if (!res.ok) {
 		throw new Error("Failed to fetch data");
@@ -67,42 +34,36 @@ export const getAnimeFromRelation = async (kitsuId: number) => {
 };
 
 export const getCharacters = async (malId: number) => {
-	const res = await fetch(`https://api.jikan.moe/v4/anime/${malId}/characters`, {
-		next: { revalidate: 2 * 60 * 60 },
+	const res = await fetch(`http://127.0.0.1:8000/anime/${malId}/characters`, {
+		cache: "no-cache",
 	});
 
 	if (!res.ok) {
 		throw new Error("Failed to fetch data");
 	}
 
-	const json = await res.json();
-	const data = json.data.sort((a: any, b: any) => b.favorites - a.favorites);
+	const data = await res.json();
 
 	return data;
 };
 
 export const getTrailer = async (malId: number) => {
-	const res = await fetch(`https://api.jikan.moe/v4/anime/${malId}/videos`, {
-		next: { revalidate: 2 * 60 * 60 },
+	const res = await fetch(`http://127.0.0.1:8000/anime/${malId}/trailer`, {
+		cache: "no-cache",
 	});
 
 	if (!res.ok) {
 		throw new Error("Failed to fetch data");
 	}
 
-	const json = await res.json();
-	const data = json.data.promo;
+	const data = await res.json();
 
-	for (const promo of data) {
-		if (promo.title.includes("PV") && !promo.title.includes("Character")) return promo;
-	}
-
-	return data?.[0];
+	return data;
 };
 
 export const getStats = async (malId: number) => {
-	const res = await fetch(`https://api.jikan.moe/v4/anime/${malId}/statistics`, {
-		next: { revalidate: 20 * 60 },
+	const res = await fetch(`http://127.0.0.1:8000/anime/${malId}/stats`, {
+		cache: "no-cache",
 	});
 
 	if (!res.ok) {
@@ -115,8 +76,8 @@ export const getStats = async (malId: number) => {
 };
 
 export const getStaff = async (malId: number) => {
-	const res = await fetch(`https://api.jikan.moe/v4/anime/${malId}/staff`, {
-		next: { revalidate: 2 * 60 * 60 },
+	const res = await fetch(`http://127.0.0.1:8000/anime/${malId}/staff`, {
+		cache: "no-cache",
 	});
 
 	if (!res.ok) {
