9ff9496752621b66a43248306d715a125afe3e3d
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-27 17:22:23 +0530

    Wrap all the sections in suspense

--- core/components/media/OverviewTab.tsx
+++ core/components/media/OverviewTab.tsx
@@ -4,22 +4,39 @@ import DetailsSection from "./DetailsSection";
 import TrailerSection from "./TrailerSection";
 import StatsSection from "./StatsSection";
 import RecommendedSection from "./RecommendedSection";
+import { Suspense } from "react";
 
 export default function OverviewTab() {
 	return (
 		<>
-			<RelationSection />
-			<CharactersSection />
+			<Suspense fallback={<p>Loading...</p>}>
+				{/* @ts-expect-error Async Server Component */}
+				<RelationSection />
+			</Suspense>
+			<Suspense fallback={<p>Loading...</p>}>
+				{/* @ts-expect-error Async Server Component */}
+				<CharactersSection />
+			</Suspense>
 
 			<div className="mt-20 grid w-[min(70rem,100%)] grid-cols-[repeat(auto-fill,30rem)] gap-x-20 gap-y-10 pr-4 md:ml-5">
-				<DetailsSection />
+				<Suspense fallback={<p>Loading...</p>}>
+					<DetailsSection />
+				</Suspense>
 				<div className="flex flex-col gap-y-10">
-					<TrailerSection />
-					<StatsSection />
+					<Suspense fallback={<p>Loading...</p>}>
+						{/* @ts-expect-error Async Server Component */}
+						<TrailerSection />
+					</Suspense>
+					<Suspense fallback={<p>Loading...</p>}>
+						{/* @ts-expect-error Async Server Component */}
+						<StatsSection />
+					</Suspense>
 				</div>
 			</div>
-
-			<RecommendedSection />
+			<Suspense fallback={<p>Loading...</p>}>
+				{/* @ts-expect-error Async Server Component */}
+				<RecommendedSection />
+			</Suspense>
 		</>
 	);
 }
--- core/components/media/StaffTab.tsx
+++ core/components/media/StaffTab.tsx
@@ -1,11 +1,18 @@
 import StaffSection from "./StaffSection";
 import VoiceActorsSection from "./VoiceActorsSection";
+import { Suspense } from "react";
 
 export default function StaffTab() {
 	return (
 		<>
-			<VoiceActorsSection />
-			<StaffSection />
+			<Suspense fallback={<p>Loading...</p>}>
+				{/* @ts-expect-error Async Server Component */}
+				<VoiceActorsSection />
+			</Suspense>
+			<Suspense fallback={<p>Loading...</p>}>
+				{/* @ts-expect-error Async Server Component */}
+				<StaffSection />
+			</Suspense>
 		</>
 	);
 }
