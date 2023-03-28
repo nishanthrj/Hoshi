501c5d7c1c57e1648a464cbc1002df15ab288acb
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-28 19:18:55 +0530

    Update MediaImage to handle landscape images

--- core/components/common/MediaImage.tsx
+++ core/components/common/MediaImage.tsx
@@ -6,7 +6,10 @@ import { useState } from "react";
 
 export default function MediaImage({ src, ...props }: ImageProps) {
 	const [error, setError] = useState(false);
-	return (
-		<Image onError={() => setError(true)} src={!error ? src : "/placeholder.png"} {...props} />
-	);
+	let placeholder = "/placeholder.png";
+	if (props.sizes) {
+		const size = parseInt(props.sizes.slice(0, -2));
+		placeholder = size >= 300 ? "/placeholderXL.png" : "/placeholder.png";
+	}
+	return <Image onError={() => setError(true)} src={!error ? src : placeholder} {...props} />;
 }
