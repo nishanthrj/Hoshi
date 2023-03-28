13114af95592413ed4b6130927d1111aeca09663
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-28 18:52:47 +0530

    Handle missing data in trailer section

--- core/components/media/TrailerSection.tsx
+++ core/components/media/TrailerSection.tsx
@@ -3,19 +3,25 @@ import { getTrailer, getAnime } from "@/utils/fetch";
 import { useMediaStore } from "@/stores/media";
 
 export default async function TrailerSection() {
-	const id = useMediaStore.getState().mediaId;
-	const media = await getAnime(id);
-	const data = await getTrailer(media.malId);
+	const id = useMediaStore.getState().malId;
+	const data = id ? await getTrailer(id) : null;
 
 	return (
 		<div>
 			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Trailer
 			</h1>
-			<Trailer
-				video={`https://www.youtube.com/embed/${data?.trailer?.youtube_id}`}
-				thumbnail={`https://img.youtube.com/vi/${data?.trailer?.youtube_id}/maxresdefault.jpg`}
-			/>
+			{data ? (
+				<Trailer
+					video={`https://www.youtube.com/embed/${data?.trailer?.youtube_id}`}
+					thumbnail={`https://img.youtube.com/vi/${data?.trailer?.youtube_id}/maxresdefault.jpg`}
+				/>
+			) : (
+				<p className="h-80 pl-4 text-base font-normal text-dark-100">
+					D-Don't think I'm doing this for you (˶ •̀ _•́ ˶), but the trailer is not
+					available.
+				</p>
+			)}
 		</div>
 	);
 }
