6e20c227a1627169b83841ca90ea0d4e6695be37
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-28 18:52:00 +0530

    Handle missing data in characters section

--- core/components/media/CharactersSection.tsx
+++ core/components/media/CharactersSection.tsx
@@ -1,39 +1,45 @@
 import CharacterCard from "@/components/media/CharacterCard";
-import { getCharacters, getAnime } from "@/utils/fetch";
+import { getCharacters } from "@/utils/fetch";
 import { useMediaStore } from "@/stores/media";
 import { v4 as uuid } from "uuid";
 
 export default async function CharactersSection() {
-	const id = useMediaStore.getState().mediaId;
-	const media = await getAnime(id);
-	const data = await getCharacters(media.malId);
+	const id = useMediaStore.getState().malId;
+	const data = id ? await getCharacters(id) : null;
 
 	return (
 		<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
 			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Characters
 			</h1>
-			<div className="flex flex-wrap gap-12">
-				{data.slice(0, 6).map((chara: any) => {
-					let va = "";
+			{data ? (
+				<div className="flex flex-wrap gap-12">
+					{data.slice(0, 6).map((chara: any) => {
+						let va = "";
 
-					for (const v of chara.voice_actors) {
-						if (v.language !== "Japanese") continue;
-						else {
-							va = v?.person.name.replace(",", "");
+						for (const v of chara.voice_actors) {
+							if (v.language !== "Japanese") continue;
+							else {
+								va = v?.person.name.replace(",", "");
+							}
 						}
-					}
 
-					return (
-						<CharacterCard
-							key={uuid()}
-							image={chara.character.images.jpg.image_url}
-							character={chara.character.name.replace(",", "")}
-							voice={va}
-						/>
-					);
-				})}
-			</div>
+						return (
+							<CharacterCard
+								key={uuid()}
+								image={chara.character.images?.jpg?.image_url}
+								character={chara.character.name.replace(",", "")}
+								voice={va}
+							/>
+						);
+					})}
+				</div>
+			) : (
+				<p className="h-32 pl-4 text-base font-normal text-dark-100">
+					D-Don't think I'm doing this for you (˶ •̀ _•́ ˶), but the characters info is not
+					available.
+				</p>
+			)}
 		</div>
 	);
 }
