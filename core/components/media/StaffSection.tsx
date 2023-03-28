6f340b16c457ac5777bf8e291b85f54289fc74d6
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-28 18:52:30 +0530

    Handle missing data in staff section

--- core/components/media/StaffSection.tsx
+++ core/components/media/StaffSection.tsx
@@ -1,28 +1,34 @@
 import StaffCard from "./StaffCard";
-import { getStaff, getAnime } from "@/utils/fetch";
+import { getStaff } from "@/utils/fetch";
 import { useMediaStore } from "@/stores/media";
 import { v4 as uuid } from "uuid";
 
 export default async function StaffSection() {
-	const id = useMediaStore.getState().mediaId;
-	const media = await getAnime(id);
-	const data = await getStaff(media.malId);
+	const id = useMediaStore.getState().malId;
+	const data = id ? await getStaff(id) : null;
 
 	return (
 		<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
 			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Staff
 			</h1>
-			<div className="grid grid-cols-[repeat(auto-fill,min(21rem,100%))] gap-8 overflow-hidden">
-				{data.map((staff: any) => (
-					<StaffCard
-						key={uuid()}
-						name={staff.person.name.replace(",", "")}
-						image={staff.person.images.jpg.image_url}
-						role={staff.positions.join(", ")}
-					/>
-				))}
-			</div>
+			{data ? (
+				<div className="grid grid-cols-[repeat(auto-fill,min(21rem,100%))] gap-8 overflow-hidden">
+					{data.map((staff: any) => (
+						<StaffCard
+							key={uuid()}
+							name={staff.person.name.replace(",", "")}
+							image={staff.person.images.jpg.image_url}
+							role={staff.positions.join(", ")}
+						/>
+					))}
+				</div>
+			) : (
+				<p className="pl-4 text-base font-normal text-dark-100">
+					D-Don't think I'm doing this for you (˶ •̀ _•́ ˶), but the staff info is not
+					available.
+				</p>
+			)}
 		</div>
 	);
 }
