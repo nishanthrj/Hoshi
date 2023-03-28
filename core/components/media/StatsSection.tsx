af80e21d7cf6ab8315067d1aca916855669a7120
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-28 18:52:38 +0530

    Handle missing data in stats section

--- core/components/media/StatsSection.tsx
+++ core/components/media/StatsSection.tsx
@@ -1,25 +1,30 @@
 import StatsCard from "@/components/media/StatsCard";
-import { getStats, getAnime } from "@/utils/fetch";
+import { getStats } from "@/utils/fetch";
 import { useMediaStore } from "@/stores/media";
 
 export default async function StatsSection() {
-	const id = useMediaStore.getState().mediaId;
-	const media = await getAnime(id);
-	const data = await getStats(media.malId);
+	const id = useMediaStore.getState().malId;
+	const data = id ? await getStats(id) : null;
 
 	return (
 		<div>
 			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Stats
 			</h1>
-			<StatsCard
-				completed={data?.completed || 0}
-				planning={data?.plan_to_watch || 0}
-				watching={data?.watching || 0}
-				paused={data?.on_hold || 0}
-				dropped={data?.dropped || 0}
-				total={data?.total || 0}
-			/>
+			{data ? (
+				<StatsCard
+					completed={data?.completed || 0}
+					planning={data?.plan_to_watch || 0}
+					watching={data?.watching || 0}
+					paused={data?.on_hold || 0}
+					dropped={data?.dropped || 0}
+					total={data?.total || 0}
+				/>
+			) : (
+				<p className="pl-4 text-base font-normal text-dark-100">
+					D-Don't think I'm doing this for you (˶ •̀ _•́ ˶), but the stats is not available.
+				</p>
+			)}
 		</div>
 	);
 }
