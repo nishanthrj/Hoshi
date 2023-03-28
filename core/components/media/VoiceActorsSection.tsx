68969cff11227d6f9a7fc6a1630e1e031767b378
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-28 18:52:59 +0530

    Handle missing data in voice actors section

--- core/components/media/VoiceActorsSection.tsx
+++ core/components/media/VoiceActorsSection.tsx
@@ -4,44 +4,50 @@ import { useMediaStore } from "@/stores/media";
 import { v4 as uuid } from "uuid";
 
 export default async function VoiceActorsSection() {
-	const id = useMediaStore.getState().mediaId;
-	const media = await getAnime(id);
-	const data = await getCharacters(media.malId);
+	const id = useMediaStore.getState().malId;
+	const data = id ? await getCharacters(id) : null;
 
 	return (
 		<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
 			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
 				Voice Actors
 			</h1>
-			<div className="grid grid-cols-[repeat(auto-fill,min(32rem,100%))] gap-8 overflow-hidden">
-				{data.map((chara: any) => {
-					const va = {
-						name: "",
-						image: "",
-						role: "",
-					};
-					for (const v of chara.voice_actors) {
-						if (v.language !== "Japanese") continue;
-						else {
-							va.name = v?.person.name.replace(",", "");
-							va.image = v?.person.images.jpg.image_url;
-							va.role = v?.language;
+			{data ? (
+				<div className="grid grid-cols-[repeat(auto-fill,min(32rem,100%))] gap-8 overflow-hidden">
+					{data.map((chara: any) => {
+						const va = {
+							name: "",
+							image: "",
+							role: "",
+						};
+						for (const v of chara.voice_actors) {
+							if (v.language !== "Japanese") continue;
+							else {
+								va.name = v?.person.name.replace(",", "");
+								va.image = v?.person.images.jpg.image_url;
+								va.role = v?.language;
+							}
 						}
-					}
 
-					return (
-						<VoiceActorCard
-							key={uuid()}
-							charaName={chara.character.name.replace(",", "")}
-							voiceName={va.name}
-							charaImage={chara.character.images.jpg.image_url}
-							voiceImage={va.image}
-							charaRole={chara.role}
-							voiceRole={va.role}
-						/>
-					);
-				})}
-			</div>
+						return (
+							<VoiceActorCard
+								key={uuid()}
+								charaName={chara.character.name.replace(",", "")}
+								voiceName={va.name}
+								charaImage={chara.character.images.jpg.image_url}
+								voiceImage={va.image}
+								charaRole={chara.role}
+								voiceRole={va.role}
+							/>
+						);
+					})}
+				</div>
+			) : (
+				<p className="h-40 pl-4 text-base font-normal text-dark-100">
+					D-Don't think I'm doing this for you (˶ •̀ _•́ ˶), but the voice actors info is
+					not available.
+				</p>
+			)}
 		</div>
 	);
 }
