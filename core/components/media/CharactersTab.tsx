2f4c57a3da9d055eab7e232869250d849778c869
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-03-29 11:23:13 +0530

    Create characters tab component

---
+++ core/components/media/CharactersTab.tsx
@@ -0,0 +1,36 @@
+import CharacterCard from "@/components/media/CharacterCard";
+import { getCharacters } from "@/utils/fetch";
+import { useMediaStore } from "@/stores/media";
+import { v4 as uuid } from "uuid";
+
+export default async function CharactersTab() {
+	const id = useMediaStore.getState().malId;
+	const data = id ? await getCharacters(id) : null;
+
+	return (
+		<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
+			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
+				Characters
+			</h1>
+			{data ? (
+				<div className="flex flex-wrap gap-12">
+					{data.map((chara: any) => {
+						return (
+							<CharacterCard
+								key={uuid()}
+								image={chara.character.images?.jpg?.image_url}
+								character={chara.character.name.replace(",", "")}
+								voice={""}
+							/>
+						);
+					})}
+				</div>
+			) : (
+				<p className="h-32 pl-4 text-base font-normal text-dark-100">
+					D-Don't think I'm doing this for you (˶ •̀ _•́ ˶), but the characters info is not
+					available.
+				</p>
+			)}
+		</div>
+	);
+}
