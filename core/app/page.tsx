22006c9816675440695b70a1d059f8021e43a84b
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-02 14:05:09 +0530

    Populate media section with placeholder data

--- core/app/page.tsx
+++ core/app/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { useNavbarStore } from "@/stores/navbar";
 import Navbar from "@/components/navbar/Navbar";
+import MediaSection from "@/components/home/MediaSection";
 
 export const metadata: Metadata = {
 	title: "Home | Hoshi",
@@ -12,6 +13,20 @@ export default function Home() {
 	return (
 		<main className="grid grid-cols-[min-content_auto]">
 			<Navbar />
+			<section className="mt-12 flex w-full flex-col items-center pl-4">
+				<div></div>
+				<div>
+					<MediaSection title="popular this season" />
+					<MediaSection title="upcoming next season" />
+					<MediaSection title="highest rated anime" />
+					<MediaSection title="most popular anime" />
+				</div>
+				<div>
+					<MediaSection title="highest rated manga" />
+					<MediaSection title="most popular manga" />
+					<MediaSection title="most popular manhwa" />
+				</div>
+			</section>
 		</main>
 	);
 }
--- core/components/home/MediaSection.tsx
+++ core/components/home/MediaSection.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import MediaCard from "@/components/home/MediaCard";
+
 interface MediaSectionProps {
 	title: string;
 	data?: any;
@@ -6,10 +9,59 @@ interface MediaSectionProps {
 export default function MediaSection({ title, data }: MediaSectionProps) {
 	return (
 		<div className="mt-20 w-[min(70rem,100%)] pr-4 md:ml-5">
-			<h1 className="mb-4 text-lg font-semibold uppercase tracking-widest text-dark-100">
-				{title}
-			</h1>
-			<div className="flex flex-wrap gap-8"></div>
+			<div className="flex items-center justify-between">
+				<h1 className="mb-4 text-lg font-semibold uppercase tracking-normal text-dark-100">
+					{title}
+				</h1>
+				<Link
+					href="/anime/top"
+					prefetch={false}
+					className="pr-6 text-xs font-semibold text-dark-200">
+					View All
+				</Link>
+			</div>
+			<div className="flex flex-wrap gap-8">
+				<MediaCard
+					id={0}
+					title={"Bocchi the Rock"}
+					slug={"bocchi-the-rock"}
+					poster={
+						"https://media.kitsu.io/anime/44196/poster_image/large-04fc7c066e52d4d2b9d0217b383597bb.jpeg"
+					}
+				/>
+				<MediaCard
+					id={0}
+					title={"Bocchi the Rock"}
+					slug={"bocchi-the-rock"}
+					poster={
+						"https://media.kitsu.io/anime/44196/poster_image/large-04fc7c066e52d4d2b9d0217b383597bb.jpeg"
+					}
+				/>
+				<MediaCard
+					id={0}
+					title={"Bocchi the Rock"}
+					slug={"bocchi-the-rock"}
+					poster={
+						"https://media.kitsu.io/anime/44196/poster_image/large-04fc7c066e52d4d2b9d0217b383597bb.jpeg"
+					}
+				/>
+				<MediaCard
+					id={0}
+					title={"Bocchi the Rock"}
+					slug={"bocchi-the-rock"}
+					poster={
+						"https://media.kitsu.io/anime/44196/poster_image/large-04fc7c066e52d4d2b9d0217b383597bb.jpeg"
+					}
+				/>
+				<MediaCard
+					id={0}
+					title={"Bocchi the Rock"}
+					slug={"bocchi-the-rock"}
+					poster={
+						"https://media.kitsu.io/anime/44196/poster_image/large-04fc7c066e52d4d2b9d0217b383597bb.jpeg"
+					}
+				/>
+			</div>
 		</div>
 	);
 }
