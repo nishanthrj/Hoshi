515c69289b1f1ecfb1a85e71e5160f3fd40cc4fe
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-08 12:56:50 +0530

    Add heading on highlight pages

--- core/app/anime/top/page.tsx
+++ core/app/anime/top/page.tsx
@@ -14,6 +14,9 @@ export default async function TopAnime() {
 		<main className="mt-8 grid grid-cols-[min-content_auto]">
 			<Navbar />
 			<section className="mt-12 w-full pl-4">
+				<h1 className="mb-4 text-center text-3xl font-bold capitalize tracking-normal text-dark-100 md:text-4xl">
+					Highest rated anime
+				</h1>
 				<HighlightSection data={data} mediaType="anime" />
 			</section>
 		</main>
--- core/app/manga/popular/page.tsx
+++ core/app/manga/popular/page.tsx
@@ -14,6 +14,9 @@ export default async function PopularManga() {
 		<main className="mt-8 grid grid-cols-[min-content_auto]">
 			<Navbar />
 			<section className="mt-12 w-full pl-4">
+				<h1 className="mb-4 text-center text-3xl font-bold capitalize tracking-normal text-dark-100 md:text-4xl">
+					Most popular manga
+				</h1>
 				<HighlightSection data={data} mediaType="anime" />
 			</section>
 		</main>
