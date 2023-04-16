78d3a522bac2effa37619d9f63b0d0a646d86288
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-16 22:27:36 +0530

    Modify logout button

--- core/components/navbar/LogoutButton.tsx
+++ core/components/navbar/LogoutButton.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { MdLogout } from "react-icons/md";
 import { signOut } from "next-auth/react";
 import { useNavbarStore } from "@/stores/navbar";
@@ -9,15 +8,14 @@ export default function LogoutButton() {
 	const isOpen = useNavbarStore((state) => state.isOpen);
 
 	return (
-		<Link
-			href="/logout"
-			className="ml-24 pt-[.85rem] transition-opacity duration-300 hover:text-dark-100 max-xs:mr-5"
+		<button
+			className="ml-24 transition-opacity duration-300 hover:text-dark-100 max-xs:mr-5"
 			onClick={() => signOut()}>
 			<MdLogout
 				className={`text-xl transition-opacity duration-300 ${
 					isOpen ? "delay-[700ms] max-xs:opacity-100" : "max-xs:opacity-0"
 				}`}
 			/>
-		</Link>
+		</button>
 	);
 }
