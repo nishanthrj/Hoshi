3125ed77c7f9d203c6d03273fb951dfb598fe111
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-17 18:25:21 +0530

    Add basic validation on register form

--- core/components/auth/InputField.tsx
+++ core/components/auth/InputField.tsx
@@ -2,9 +2,21 @@ interface InputFieldProps {
 	name: string;
 	type: string;
 	text: string;
+	value?: string;
+	error?: string;
+	onChange?: React.FocusEventHandler<HTMLInputElement>;
+	onBlur?: React.FocusEventHandler<HTMLInputElement>;
 }
 
-export default function InputField({ name, type, text }: InputFieldProps) {
+export default function InputField({
+	name,
+	type,
+	text,
+	value,
+	error,
+	onChange,
+	onBlur,
+}: InputFieldProps) {
 	return (
 		<>
 			<label htmlFor={name} className="font-bold">
@@ -13,8 +25,12 @@ export default function InputField({ name, type, text }: InputFieldProps) {
 			<input
 				type={type}
 				name={name}
-				className="mb-8 mt-2 h-10 rounded-md border-none bg-dark-400 p-3 text-sm font-bold text-dark-50"
+				onChange={onChange}
+				onBlur={onBlur}
+				value={value}
+				className="mt-2 h-10 rounded-md border-none bg-dark-400 p-3 text-sm font-bold text-dark-50"
 			/>
+			<span className="mb-8 mt-2 text-xs text-red-400">{error}</span>
 		</>
 	);
 }
--- core/components/auth/RegisterForm.tsx
+++ core/components/auth/RegisterForm.tsx
@@ -1,14 +1,76 @@
 "use client";
 
+import * as Yup from "yup";
 import InputField from "./InputField";
+import { useFormik } from "formik";
 
 export default function RegisterWrapper() {
+	const formik = useFormik({
+		initialValues: {
+			username: "",
+			email: "",
+			password: "",
+			conpassword: "",
+		},
+
+		validationSchema: Yup.object({
+			username: Yup.string()
+				.min(5, "Username must be at least 5 characters")
+				.max(30, "Username must be at most 30 characters")
+				.matches(/^\w+$/, "Username must be alphanumeric.")
+				.required("Enter your username"),
+			email: Yup.string().email("Enter a valid email").required("Enter your email"),
+			password: Yup.string()
+				.min(8, "Password must be at least 8 characters")
+				.required("Enter a password"),
+			conpassword: Yup.string()
+				.oneOf([Yup.ref("password")], "Passwords does not match")
+				.required("Confirm your password"),
+		}),
+
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
+
 	return (
 		<form method="POST" className="flex flex-col">
-			<InputField name="username" type="text" text="Username" />
-			<InputField name="email" type="email" text="E-Mail" />
-			<InputField name="password" type="password" text="Password" />
-			<InputField name="conpassword" type="password" text="Confirm Password" />
+			<InputField
+				name="username"
+				type="text"
+				text="Username"
+				value={formik.values.username}
+				error={formik.touched.username ? formik.errors.username : ""}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+			/>
+			<InputField
+				name="email"
+				type="email"
+				text="E-Mail"
+				value={formik.values.email}
+				error={formik.touched.email ? formik.errors.email : ""}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+			/>
+			<InputField
+				name="password"
+				type="password"
+				text="Password"
+				value={formik.values.password}
+				error={formik.touched.password ? formik.errors.password : ""}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+			/>
+			<InputField
+				name="conpassword"
+				type="password"
+				text="Confirm Password"
+				value={formik.values.conpassword}
+				error={formik.touched.conpassword ? formik.errors.conpassword : ""}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+			/>
 			<button
 				type="submit"
 				className="mb-8 mt-3 h-12 rounded-md border-none bg-dark-400 p-3 text-lg font-bold text-dark-50 transition-all duration-300 hover:brightness-125">
