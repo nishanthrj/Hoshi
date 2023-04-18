2c032a5a21b177b4e078a048f4fbf0805f6ac5dc
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-18 22:14:30 +0530

    Add link to the logo on auth pages

--- core/app/(auth)/register/page.tsx
+++ core/app/(auth)/register/page.tsx
@@ -13,15 +13,17 @@ export default async function Register() {
 
 	return (
 		<main className="flex w-full flex-col items-center justify-center p-2">
-			<Image
-				src="/logo.svg"
-				width={120}
-				height={72}
-				quality={100}
-				priority={true}
-				alt="Hoshi Logo"
-				className="mb-2 h-[5.5rem] w-[7.5rem]"
-			/>
+			<Link href="/" prefetch={false}>
+				<Image
+					src="/logo.svg"
+					width={120}
+					height={72}
+					quality={100}
+					priority={true}
+					alt="Hoshi Logo"
+					className="mb-2 h-[5.5rem] w-[7.5rem]"
+				/>
+			</Link>
 			<div className="mb-12 w-[min(30rem,100%)] rounded-lg border border-dark-300 bg-dark-700 p-8 text-dark-100">
 				<h1 className="mb-8 text-center text-2xl font-light uppercase tracking-widest">
 					Sign up
