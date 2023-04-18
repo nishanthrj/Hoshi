c5b414d1fb8ba862f338bdf69a91e1b1cbae540e
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-18 21:41:06 +0530

    Rename NavIcon to MenuIcon

--- core/components/navbar/MenuIcon.tsx
+++ core/components/navbar/MenuIcon.tsx
@@ -3,12 +3,12 @@
 import { Spiral as Hamburger } from "hamburger-react";
 import { useNavbarStore } from "@/stores/navbar";
 
-export default function NavIcon(): JSX.Element {
+export default function MenuIcon(): JSX.Element {
 	const isOpen = useNavbarStore((state) => state.isOpen);
 	const toggleNavbar = useNavbarStore((state) => state.toggleNavbar);
 
 	return (
-		<div className="absolute top-0 left-0 z-40 bg-transparent p-2 md:fixed">
+		<div className="absolute left-0 top-0 z-40 bg-transparent p-2 md:fixed">
 			<Hamburger
 				toggled={isOpen}
 				toggle={toggleNavbar}
