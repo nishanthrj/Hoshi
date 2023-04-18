e6a6eb2c9a492391442fcb0821470297f32b0e53
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-18 19:38:46 +0530

    Show a message on pages under development

---
+++ core/app/(user)/settings/page.tsx
@@ -0,0 +1,29 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export default async function Settings() {
+	const session = await getServerSession(authOptions);
+
+	if (session?.user)
+		return (
+			<div className="absolute inset-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-dark p-4">
+				<Image
+					src="/dev.png"
+					width={500}
+					height={500}
+					quality={100}
+					priority={true}
+					alt=""
+					className="h-64 w-64"
+				/>
+				<h1 className="mt-12 text-center text-xl font-bold text-dark-100">
+					We&apos;re currently gathering cosmic materials to form this page.
+				</h1>
+			</div>
+		);
+	else {
+		redirect("/");
+	}
+}
