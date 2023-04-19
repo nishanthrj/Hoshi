e1862b2275dc3079f72b320d2060b7c3b3563250
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-19 11:44:29 +0530

    Add title in upcoming anime page

--- core/app/(media)/anime/next-season/page.tsx
+++ core/app/(media)/anime/next-season/page.tsx
@@ -12,6 +12,9 @@ export default async function NextSeason() {
 	return (
 		<main className="mt-8">
 			<section className="mt-12 w-full pl-4">
+				<h1 className="mb-4 text-center text-3xl font-bold capitalize tracking-normal text-dark-100 md:text-4xl">
+					Upcoming Next Season
+				</h1>
 				<HighlightSection data={data} mediaType="anime" />
 			</section>
 		</main>
