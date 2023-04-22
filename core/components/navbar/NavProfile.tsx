aeaa9a2d20305a9f7279d6e1586cbfc3b9018237
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-22 22:27:42 +0530

    Modify navbar to use session data

--- core/components/navbar/NavProfile.tsx
+++ core/components/navbar/NavProfile.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import LogoutButton from "@/components/navbar/LogoutButton";
 
-export default async function NavProfile() {
+interface NavProfileProps {
+	username: string;
+	avatar: string | null;
+}
+
+export default async function NavProfile({ username, avatar }: NavProfileProps) {
 	return (
 		<div className="mt-auto flex border-t border-dark-100/25 pt-6 max-xs:justify-between">
 			<div className="inline-flex gap-3 pl-2 font-semibold leading-[3rem] text-dark-100 hover:text-dark-100">
 				<div className="relative h-10 w-10">
 					<Image
-						src={"/default.png"}
+						src={avatar ? avatar : "/default.png"}
 						fill={true}
 						quality={100}
 						loading={"eager"}
@@ -15,7 +20,7 @@ export default async function NavProfile() {
 						className="object-contain"
 					/>
 				</div>
-				Guest
+				{username}
 			</div>
 			<LogoutButton />
 		</div>
--- core/components/navbar/Navbar.tsx
+++ core/components/navbar/Navbar.tsx
@@ -1,21 +1,53 @@
 import { MdHome, MdSettings, MdSearch, MdPlayArrow, MdMenuBook, MdAutoGraph } from "react-icons/md";
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { headers, cookies } from "next/headers";
+import { Database } from "@/types/db";
 import NavLink from "@/components/navbar/NavLink";
 import NavProfile from "@/components/navbar/NavProfile";
 import NavbarWrapper from "@/components/navbar/NavbarWrapper";
 import LoginButton from "@/components/navbar/LoginButton";
 
 export default async function Navbar() {
+	const supabase = createServerComponentSupabaseClient<Database>({
+		headers,
+		cookies,
+	});
+
+	const { data } = await supabase.auth.getSession();
+	const username = data.session?.user.user_metadata.username;
+	const avatar = data.session?.user.user_metadata.avatar;
+
 	return (
 		<NavbarWrapper>
 			<nav className="ml-2 mt-8 flex h-[85vh] select-none flex-col whitespace-nowrap text-dark-200 transition-colors duration-300">
 				<NavLink name="Home" path="/" Icon={MdHome} />
 				<NavLink name="Search" path="/search" Icon={MdSearch} />
-				<NavLink name="AnimeList" path={`/user/animelist`} Icon={MdPlayArrow} />
-				<NavLink name="MangaList" path={`/user/mangalist`} Icon={MdMenuBook} />
-				<NavLink name="Stats" path={`/user/stats`} Icon={MdAutoGraph} />
-				<NavLink name="Settings" path="/settings" Icon={MdSettings} />
+				<NavLink
+					name="AnimeList"
+					path={`/${username}/animelist`}
+					Icon={MdPlayArrow}
+					disabled={!username ? true : false}
+				/>
+				<NavLink
+					name="MangaList"
+					path={`/${username}/mangalist`}
+					Icon={MdMenuBook}
+					disabled={!username ? true : false}
+				/>
+				<NavLink
+					name="Stats"
+					path={`/${username}/stats`}
+					Icon={MdAutoGraph}
+					disabled={!username ? true : false}
+				/>
+				<NavLink
+					name="Settings"
+					path="/settings"
+					Icon={MdSettings}
+					disabled={!username ? true : false}
+				/>
 				{/* @ts-expect-error Async Server Component */}
-				<NavProfile />
+				{username ? <NavProfile username={username} avatar={avatar} /> : <LoginButton />}
 			</nav>
 		</NavbarWrapper>
 	);
