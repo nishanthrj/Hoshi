48d950784dbfc370eb5465e0a9ef7f6ab2177715
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-22 22:31:13 +0530

    Protect user pages from unauthorized users

--- core/middleware.ts
+++ core/middleware.ts
@@ -1,10 +1,23 @@
 import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse, NextRequest } from "next/server";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { Database } from "@/types/db";
+
+const protectedPages = ["/animelist", "/mangalist", "/settings", "/stats"];
 
 export async function middleware(req: NextRequest) {
 	const res = NextResponse.next();
+
 	const supabase = createMiddlewareSupabaseClient<Database>({ req, res });
-	await supabase.auth.getSession();
+
+	const {
+		data: { session },
+	} = await supabase.auth.getSession();
+
+	if (!session && protectedPages.some((page) => req.nextUrl.pathname.includes(page))) {
+		redirect("/");
+	}
+
 	return res;
 }
