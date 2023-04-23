464c73117840f0616caec5d8beab2e8ae65e6c81
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-04-23 22:03:42 +0530

    Handle expired link error in verified page

---
+++ core/app/(auth)/auth/verified/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function Verfified() {
+	if (!window.location.href.includes("error")) {
+		setTimeout(() => {
+			window.location.href = "/";
+		}, 3000);
+		return (
+			<div className="absolute inset-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-dark-900/95 text-dark-100 backdrop-blur">
+				<h1 className="text-3xl font-bold">Account verified successfully!</h1>
+				<p className="mt-2 text-sm">You will be redirected to the home page shortly.</p>
+			</div>
+		);
+	} else {
+		return (
+			<div className="absolute inset-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-dark-900/95 text-dark-100 backdrop-blur">
+				<h1 className="text-3xl font-bold">Account verfication failed!</h1>
+				<p className="mt-2 text-sm">Your email link is invalid or expired.</p>
+			</div>
+		);
+	}
+}
