486082770e69d3f2cfb74b645223e3a1f4e46cea
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-05-04 13:18:27 +0530

    Include poster as the link in mini media card

--- core/components/home/MediaCardMini.tsx
+++ core/components/home/MediaCardMini.tsx
@@ -12,22 +12,21 @@ interface MediaCardMiniProps {
 export default function MediaCardMini({ id, slug, title, poster, mediaType }: MediaCardMiniProps) {
 	return (
 		<div>
-			<div className="relative aspect-[4/6]">
-				<Image
-					src={poster ? poster : ""}
-					fill={true}
-					style={{ objectFit: "cover" }}
-					quality={80}
-					sizes="340px"
-					alt="cover"
-					className="rounded-sm"
-				/>
-			</div>
-			<Link
-				href={`/${mediaType}/${id}/${slug}`}
-				prefetch={false}
-				className="mt-2 line-clamp-2 text-sm font-semibold text-dark-100 max-xs:text-xs">
-				{title}
+			<Link href={`/${mediaType}/${id}/${slug}`} prefetch={false}>
+				<div className="relative aspect-[4/6]">
+					<Image
+						src={poster ? poster : ""}
+						fill={true}
+						style={{ objectFit: "cover" }}
+						quality={80}
+						sizes="340px"
+						alt="cover"
+						className="rounded-sm"
+					/>
+				</div>
+				<p className="mt-2 line-clamp-2 text-sm font-semibold text-dark-100 max-xs:text-xs">
+					{title}
+				</p>
 			</Link>
 		</div>
 	);
